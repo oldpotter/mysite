e7c722955046aecad6f8cd19832c8b36d783cbb0
SKL <oldpottertom@icloud.com>
2017-03-01 16:40:17 +0800

    可以投票看结果了

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Question
+from django.http import HttpResponse,HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Question,Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -11,3 +13,27 @@ def index(request):
 def detail(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html',{'question':question})
+
+def vote(request,question_id):
+    return HttpResponse("给问题%s投票了"%question_id)
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message':'你没有选择',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{
+        'question':question
+    })
