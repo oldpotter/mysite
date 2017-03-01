98aae4037a887242fcb204feab205969fa957afb
SKL <oldpottertom@icloud.com>
2017-03-01 14:26:44 +0800

    url相关

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import  Http404
+from django.shortcuts import render,get_object_or_404
 from .models import Question
 
 def index(request):
@@ -10,8 +9,5 @@ def index(request):
     return render(request,'polls/index.html',context)
 
 def detail(request,question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("问题没有找到")
+    question = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html',{'question':question})
